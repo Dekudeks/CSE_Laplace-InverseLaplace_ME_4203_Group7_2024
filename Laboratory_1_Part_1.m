bdedaadd425f9d97d4eda2da03c22d17ccd3d571
Mae Malaluan <159032350+msmlln@users.noreply.github.com>
2024-02-27 08:42:29 +0800

    Add files via upload

---
+++ Laboratory_1_Part_1.m
@@ -0,0 +1,20 @@
+%Clear
+clear
+clc
+close all
+
+syms t s
+
+%% I. Solving for the Laplace Transform
+
+F1_s = 3-exp(-3*t)+5*sin(2*t);
+f1_t = laplace(F1_s)
+pretty(f1_t)
+
+F2_s = 3+12*t+42*(t^3)-3*exp(2*t);
+f2_t = laplace(F2_s)
+pretty(f2_t)
+
+F3_s = (t+1)*(t+2);
+f3_t = laplace(F3_s)
+pretty(f3_t)
